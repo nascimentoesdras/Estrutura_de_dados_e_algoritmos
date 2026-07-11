e64ca2239414e83eb4a403f3bff319c53a175fd9
Esdras Nascimento <esdrashernannym@gmail.com>
2026-07-11 11:36:02 -0300

    feat: Variables in java.

--- src/HelloWorldInJava.java
+++ src/HelloWorldInJava.java
@@ -1,5 +1,10 @@
 public class HelloWorldInJava {
     public static void main(String[] args){
         System.out.println("Olá Esdras! seja bem-vindo ao Java!");
+        /* In java, we can use de same method to print Numbers
+        / like the exemple below:
+        */
+
+        System.out.println("My age is " + 37);
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        * In Java, to declaring variables we have to start declaring the data type
+        * that is required to our necessities.
+        *
+        * They must be String, char, int, float or boolean.
+        *
+        * Note that the String type starts with uppercase because it is treated
+        * like a class, and like other classes have your own methods.
+        *
+        * Let's now write our variables and print her outputs.
+        * */
+
+        String myString = "Isto é uma String em Java!";
+        char myChar = 'A';
+        int myInt = 123;
+        float myFloat = 123.4f;
+        boolean myBool = true; // or 'false'
+
+
+        // Saídas de cada tipo:
+
+        System.out.println(myString);
+        System.out.println(myChar);
+        System.out.println(myInt);
+        System.out.println(myFloat);
+        System.out.println(myBool);
+
+    }
+}
