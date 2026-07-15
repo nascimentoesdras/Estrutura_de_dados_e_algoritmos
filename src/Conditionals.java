76c403d2bfbdd91edd36951e3df30e8094f09df4
Esdras Nascimento <esdrashernannym@gmail.com>
2026-07-15 12:59:22 -0300

    feat: Add conditionals.java

---
+++ src/Conditionals.java
@@ -0,0 +1,16 @@
+public class Conditionals {
+    public static void main(String[] args){
+        /*in java, we have also conditionals structures that allows get control
+         to the data flux in our program.
+        */
+
+        // The "if" statement is used to validate if some data is true or false:
+        String name = "kkk";
+
+        if() {
+            System.out.println("The variable is empty");
+        }else {
+            System.out.println("Yes, my name is Esdras!");
+        }
+    }
+}
