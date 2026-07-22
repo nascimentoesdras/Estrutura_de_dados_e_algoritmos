d7ba4c4e04d887cc5a1649620863f41a15eab5e8
Esdras Nascimento <esdrashernannym@gmail.com>
2026-07-22 14:18:47 -0300

    feat(Operators): implemented new examples of arithmetics operators ("-", "*")

--- src/Operators.java
+++ src/Operators.java
@@ -24,7 +24,18 @@ public class Operators {
 
         System.out.println("Second example: make an addition between a variable a a static number: " + (addXY + 20));
 
+        // The "-" operator
+        // We can use it to make a subtraction operation.
 
+        y = 5; // change the variable value to match with the operation bellow
 
+        int subXY = x - y;
+        System.out.println("In this example we have a substraction operation and this result is: " + subXY);
+
+        // The "*" operator
+        // we use this to make a multiplication between two or more variables.
+
+        int multiXY = x * y;
+        System.out.println("This is a multiplication between X and Y variables and this result is " + multiXY);
     }
 }
